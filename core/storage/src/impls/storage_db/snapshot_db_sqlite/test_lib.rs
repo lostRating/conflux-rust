42fd5a7a58bc46883540c95a88f0b6a51a93c43a
Yao Li <rauledly@gmail.com>
2020-09-29 18:16:18 +0800

    add rocksdb

--- core/storage/src/impls/storage_db/snapshot_db_sqlite/test_lib.rs
+++ core/storage/src/impls/storage_db/snapshot_db_sqlite/test_lib.rs
@@ -3,6 +3,135 @@
 // See http://www.gnu.org/licenses/
 
 #[cfg(test)]
+impl KeyValueDbTraitOwnedRead for KvdbRocksdb {
+    fn get_mut(&mut self, _key: &[u8]) -> Result<Option<Box<[u8]>>> {
+        unreachable!()
+    }
+}
+
+#[cfg(test)]
+impl<'db> OpenSnapshotMptTrait<'db> for KvdbRocksdb {
+    type SnapshotDbAsOwnedType = SnapshotMpt<
+        KvdbSqliteSharded<SnapshotMptDbValue>,
+        KvdbSqliteSharded<SnapshotMptDbValue>,
+    >;
+    type SnapshotDbBorrowMutType = SnapshotMpt<
+        KvdbSqliteSharded<SnapshotMptDbValue>,
+        KvdbSqliteSharded<SnapshotMptDbValue>,
+    >;
+    type SnapshotDbBorrowSharedType = SnapshotMpt<
+        KvdbSqliteSharded<SnapshotMptDbValue>,
+        KvdbSqliteSharded<SnapshotMptDbValue>,
+    >;
+
+    fn open_snapshot_mpt_owned(
+        &'db mut self,
+    ) -> Result<Self::SnapshotDbBorrowMutType> {
+        unreachable!();
+    }
+
+    fn open_snapshot_mpt_as_owned(
+        &'db self,
+    ) -> Result<Self::SnapshotDbAsOwnedType> {
+        unreachable!();
+    }
+
+    fn open_snapshot_mpt_shared(
+        &'db self,
+    ) -> Result<Self::SnapshotDbBorrowSharedType> {
+        unreachable!();
+    }
+}
+
+#[cfg(test)]
+impl KeyValueDbTraitSingleWriter for KvdbRocksdb {
+    fn delete(&mut self, _key: &[u8]) -> Result<Option<Option<Box<[u8]>>>> {
+        unreachable!();
+    }
+
+    fn put(
+        &mut self, key: &[u8], value: &[u8],
+    ) -> Result<Option<Option<Box<[u8]>>>> {
+        let mut transaction = self.kvdb.transaction();
+        transaction.put(self.col, key, value);
+        self.kvdb.write(transaction)?;
+        Ok(None)
+    }
+}
+
+#[cfg(test)]
+impl SnapshotDbTrait for KvdbRocksdb {
+    type SnapshotKvdbIterTraitTag = KvdbSqliteShardedIteratorTag;
+    type SnapshotKvdbIterType =
+        KvdbSqliteSharded<Box<[u8]>>;
+
+    fn get_null_snapshot() -> Self {
+        unreachable!();
+    }
+
+    fn open(
+        snapshot_path: &Path, _readonly: bool,
+        already_open_snapshots: &AlreadyOpenSnapshots<Self>,
+        open_semaphore: &Arc<Semaphore>,
+    ) -> Result<KvdbRocksdb>
+    {
+        Self::create(
+            snapshot_path,
+            already_open_snapshots,
+        open_semaphore)
+    }
+
+    fn create(
+        snapshot_path: &Path,
+        _already_open_snapshots: &AlreadyOpenSnapshots<Self>,
+        _open_snapshots_semaphore: &Arc<Semaphore>,
+    ) -> Result<KvdbRocksdb>
+    {
+        Ok(KvdbRocksdb {
+            kvdb: Arc::new(Database::open_default(
+                snapshot_path.to_str().unwrap(),
+            )?),
+            col: 0,
+        })
+    }
+
+    fn direct_merge(&mut self) -> Result<MerkleHash> {
+        unreachable!();
+    }
+
+    fn copy_and_merge(
+        &mut self, _old_snapshot_db: &KvdbRocksdb,
+    ) -> Result<MerkleHash> {
+        unreachable!();
+    }
+
+    fn start_transaction(&mut self) -> Result<()> {
+        Ok(())
+    }
+
+    fn commit_transaction(&mut self) -> Result<()> {
+        Ok(())
+    }
+
+    fn snapshot_kv_iterator(
+        &self,
+    ) -> Result<
+        Wrap<
+            KvdbSqliteSharded<Box<[u8]>>,
+            dyn KeyValueDbIterableTrait<
+                MptKeyValue,
+                [u8],
+                KvdbSqliteShardedIteratorTag,
+            >,
+        >,
+    > {
+        unreachable!();
+    }
+}
+
+trait A : Sized {}
+
+#[cfg(test)]
 #[test]
 pub fn test_db_size() {
     fn dir_size(path: impl Into<PathBuf>) -> io::Result<u64> {
@@ -27,17 +156,18 @@ pub fn test_db_size() {
     let kv_keys_per_round = 1000000;
     let code_keys_per_found = 10000;
     let exp_params = vec![
-        ("kv", 10, kv_keys_per_round),
-        ("kv", 30, kv_keys_per_round),
-        ("kv", 50, kv_keys_per_round),
-        ("code", 50_000, code_keys_per_found),
-        ("code", 100_000, code_keys_per_found),
-        ("code", 150_000, code_keys_per_found),
+        //("snapshot_db", "kv", 10, kv_keys_per_round),
+        ("snapshot_db", "kv", 30, kv_keys_per_round),
+        ("snapshot_db", "kv", 50, kv_keys_per_round),
+        ("snapshot_db", "code", 5_000, code_keys_per_found),
+        ("snapshot_db", "code", 10_000, code_keys_per_found),
+        ("snapshot_db", "code", 15_000, code_keys_per_found),
+        ("snapshot_db", "code", 20_000, code_keys_per_found),
     ];
     const ROUNDS: u32 = 10;
 
     for exp_params in &exp_params {
-        let (exp_type, data_len, kvs_per_round) = exp_params;
+        let (db_type, exp_type, data_len, kvs_per_round) = exp_params;
         println!(
             "start experiment {} data len {} kvs_per_round {}",
             exp_type, data_len, kvs_per_round
@@ -46,18 +176,17 @@ pub fn test_db_size() {
             Default::default();
         let open_snapshot_semaphore: Arc<Semaphore> =
             Arc::new(Semaphore::new(1 as usize));
-        let db = SnapshotDbSqlite::create(
-            Path::new(&sql_path),
-            &already_open_snapshots,
-            &open_snapshot_semaphore,
-        )
-        .unwrap();
+            let db = SnapshotDbSqlite::create(
+                Path::new(&sql_path),
+                &already_open_snapshots,
+                &open_snapshot_semaphore,
+            )
+            .unwrap();
         drop(db);
         println!("init {}", dir_size(&PATH_STR).unwrap());
         for _i in 0..ROUNDS {
-            let mut db = SnapshotDbSqlite::open(
+            let mut db = SnapshotDbSqlite::create(
                 Path::new(&sql_path),
-                false,
                 &already_open_snapshots,
                 &open_snapshot_semaphore,
             )
@@ -99,9 +228,8 @@ pub fn test_db_size() {
             drop(db);
             println!("round {}: {}", _i, dir_size(&PATH_STR).unwrap());
         }
+        fs::remove_dir_all(PATH_STR).expect("remove dir");
     }
-
-    fs::remove_dir_all(PATH_STR).expect("remove dir");
 }
 
 #[cfg(test)]
@@ -178,7 +306,10 @@ use crate::{
             mpt_cursor::{BasicPathNode, CursorOpenPathTerminal, MptCursor},
             TrieNodeTrait,
         },
-        storage_db::snapshot_db_sqlite::SnapshotDbSqlite,
+        storage_db::{
+            kvdb_sqlite_sharded::KvdbSqliteSharded,
+            snapshot_db_sqlite::SnapshotDbSqlite,
+        }
     },
     storage_db::{snapshot_db::OpenSnapshotMptTrait, SnapshotMptTraitRead},
     utils::access_mode,
@@ -189,19 +320,34 @@ use std::fmt::Debug;
 #[cfg(test)]
 use crate::impls::{
     defaults::DEFAULT_MAX_OPEN_SNAPSHOTS,
+    merkle_patricia_trie::MptKeyValue,
     storage_db::{
+        kvdb_rocksdb::KvdbRocksdb,
+        kvdb_sqlite_sharded::KvdbSqliteShardedIteratorTag,
         snapshot_db_manager_sqlite::AlreadyOpenSnapshots,
         snapshot_db_sqlite::SnapshotDbTrait,
+        snapshot_mpt::SnapshotMpt,
     },
 };
 #[cfg(test)]
-use crate::storage_db::KeyValueDbTraitSingleWriter;
+use crate::{
+    storage_db::{
+        KeyValueDbIterableTrait,
+        KeyValueDbTraitOwnedRead,
+        KeyValueDbTraitSingleWriter,
+        KeyValueDbTypes,
+        snapshot_mpt::SnapshotMptDbValue,
+    },
+    utils::wrap::Wrap,
+};
 #[cfg(test)]
 use cfx_types::{Address, H256};
 #[cfg(test)]
 use cfxstore::random_string;
 #[cfg(test)]
-use primitives::{CodeInfo, StorageKey};
+use kvdb_rocksdb::Database;
+#[cfg(test)]
+use primitives::{CodeInfo, MerkleHash, StorageKey};
 #[cfg(test)]
 use std::{
     fs, io,
